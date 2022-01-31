ada6ce10c20d686cb91659ff2cb87bb994d93c5f
Matt Mannion <mattpmannion@gmail.com>
2022-01-31 07:24:58 -0500

    test

--- src/server/ipc/channel_one.ts
+++ src/server/ipc/channel_one.ts
@@ -2,5 +2,5 @@ import { ipcMain } from 'electron';
 import { channel_one, channel_two } from 'util/ipc_registry';
 
 ipcMain.on(channel_one, (e) => {
-  e.sender.send(channel_two, 'message message');
+  e.sender.send(channel_two, 'message m');
 });
