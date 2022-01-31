efb57ef0c3e58320763ca864704cd332d0c3a15c
Matt Mannion <mattpmannion@gmail.com>
2022-01-31 07:31:10 -0500

    npm-watch config

--- src/server/ipc/channel_one.ts
+++ src/server/ipc/channel_one.ts
@@ -2,9 +2,5 @@ import { ipcMain } from 'electron';
 import { channel_one, channel_two } from 'util/ipc_registry';
 
 ipcMain.on(channel_one, (e) => {
-<<<<<<< HEAD
   e.sender.send(channel_two, 'message m');
-=======
-  e.sender.send(channel_two, JSON.stringify({ a: 'one', b: 'two' }));
->>>>>>> 1a1f9fe2edfa5f850bf879c9a8a9eb11ecd47ed8
 });
