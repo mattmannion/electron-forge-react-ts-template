167d80334ecd727d2a5fd0bb81c4f060b65fc3df
Matt Mannion <mattpmannion@gmail.com>
2022-07-08 17:12:13 -0400

    added cleanup for listeners

--- src/server/ipc/channel_one.ts
+++
@@ -1,6 +0,0 @@
-import { ipcMain } from 'electron';
-import { channel_one, channel_two } from 'util/ipc_registry';
-
-ipcMain.on(channel_one, (e) => {
-  e.sender.send(channel_two, 'message m');
-});
